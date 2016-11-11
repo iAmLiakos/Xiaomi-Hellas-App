b33c629fff79b29dbdda0d36f706925aed36da5f
iamliakos <papaniki90@gmail.com>
2016-11-11 12:29:23 +0200

    Initial - 7th edition
    Added “loading” message
    Added Xiaomi Phones list
    Added rounded buttons

---
+++ XiaomiMIUIHellas/obj/Debug/android/src/md55280b62bb42226b96526222f7d343aef/MiPhones.java
@@ -0,0 +1,56 @@
+package md55280b62bb42226b96526222f7d343aef;
+
+
+public class MiPhones
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onBackPressed:()V:GetOnBackPressedHandler\n" +
+			"";
+		mono.android.Runtime.register ("XiaomiMIUIHellas.MiPhones, XiaomiMIUIHellas, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MiPhones.class, __md_methods);
+	}
+
+
+	public MiPhones () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MiPhones.class)
+			mono.android.TypeManager.Activate ("XiaomiMIUIHellas.MiPhones, XiaomiMIUIHellas, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onBackPressed ()
+	{
+		n_onBackPressed ();
+	}
+
+	private native void n_onBackPressed ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
